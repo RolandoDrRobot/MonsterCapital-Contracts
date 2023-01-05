518711ddf5d033f08a06644566ebc1751b978e43
RolandoDrRobot <rolando.arguello.email@gmail.com>
2023-01-05 11:19:07 -0600

    Contracts up to date

--- README.md
+++ README.md
@@ -19,9 +19,20 @@
 
 
 ## ⚙️ How to install
+CoraToken deployed to: 0x3217F75161BAB18F75567dD3E85BFE842FcF46F0
+MysticAnimals deployed to: 0xDfB72b74DD0bBeA7C9e86fc2469c8706e5493D73
+Tribe Masks deployed to: 0x84E6B23a9f8eD4f200198D81B519251f14ef6A0d
 
+CoraToken Mumbai: 0x31474D08FA9Eefb50693e43729b52e1B8f01aA6E
+MysticAnimals deployed to: 0xdC4A1b9Ecf774Ae840E75C0ce07443B14c1b1391
+Tribe Masks deployed to: 0x6ed8C5d0e61437FEe80Bda983216948E4242d796
 
 ## ⚙️ How to use
 
 
 ## 📗 Improvements Ideas
+
+
+## 📷 Screenshots
+
+### Collection
--- hardhat.config.js
+++ hardhat.config.js
@@ -5,11 +5,35 @@ const privateKey = process.env.DEPLOYER_SIGNER_PRIVATE_KEY;
 const projectID = process.env.INFURA_PROJECT_ID;
 
 module.exports = {
-  solidity: "0.8.0",
+  defaultNetwork: 'mumbai',
   networks: {
-    rinkeby: {
-      url: `https://rinkeby.infura.io/v3/${projectID}`,
+    hardhat: {
+    },
+    matic: {
+      url: `https://polygon-mainnet.g.alchemy.com/v2/${projectID}`,
+      accounts: [privateKey],
+    },
+    mumbai: {
+      url: `https://polygon-mumbai.g.alchemy.com/v2/${projectID}`,
       accounts: [privateKey],
     }
+  },
+  solidity: {
+    version: "0.8.0",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  },
+  mocha: {
+    timeout: 20000
   }
 };
